0c56965f75a5627175f31e8dd17263e6c119585f
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2022-12-01 23:49:11 +0000

    Day 1 AoC 2022
    update: combine tests with 'real' code file

--- day01/day01.py
+++ day01/day01.py
@@ -10,6 +10,16 @@ def count_calories(test_file, top_n=1):
     return sum(sorted(calorie_totals)[-top_n:])
 
 
+def test__calorie_count():
+    expected = 24000
+    assert count_calories('day01_test_input.txt') == expected
+
+
+def test__top_three_calorie_count():
+    expected = 45000
+    assert count_calories('day01_test_input.txt', 3) == expected
+
+
 if __name__ == '__main__':
     print("Elf with most calories has: {}".format(count_calories('day01_real_input.txt')))
     print("Top 3 most calorie carrying elves have {}".format(count_calories('day01_real_input.txt', 3)))
