657e5b4c499036aeaa244bcc4f137fb2d79edb4b
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2022-12-10 00:57:59 +0000

    Day 9 AoC 2022
    part 1.1

--- day09/day09.py
+++ day09/day09.py
@@ -1,8 +1,6 @@
-
-def perform_move(head, tail, move):
+def move_head(head, move):
     assert move in ('L', 'R', 'U', 'D')
     hx, hy = head
-    tx, ty = tail
 
     if move == 'L':
         hx -= 1
@@ -13,6 +11,12 @@ def perform_move(head, tail, move):
     else:  # move == 'D':
         hy -= 1
 
+    return [hx, hy]
+
+
+def move_tail(head, tail):
+    hx, hy = head
+    tx, ty = tail
     if abs(hx - tx) + abs(hy - ty) == 3:
         tx = tx + 1 if hx > tx else tx - 1
         ty = ty + 1 if hy > ty else ty - 1
@@ -25,8 +29,7 @@ def perform_move(head, tail, move):
         # tail must move vertically towards head
     else:
         pass  # nothing needs to move
-
-    return [hx, hy], [tx, ty]
+    return [tx, ty]
 
 
 def calculate_tail_positions(input_file):
@@ -41,26 +44,31 @@ def calculate_tail_positions(input_file):
     for move in moves:
         move_direction, move_count = move.strip().split()
         for _ in range(int(move_count)):
-            head_position, tail_position = perform_move(head_position, tail_position, move_direction)
+            head_position = move_head(head_position, move_direction)
+            tail_position = move_tail(head_position, tail_position)
             tail_positions.add((tail_position[0], tail_position[1]))
     return tail_positions
 
 
 def test__perform_move():
-    head, tail = perform_move([0, 0], [0, 0], 'R')
+    head = move_head([0, 0], 'R')
     assert head == [1, 0]
+    tail = move_tail(head, [0, 0])
     assert tail == [0, 0]
 
-    head, tail = perform_move([1, 0], [0, 0], 'R')
+    head = move_head([1, 0], 'R')
     assert head == [2, 0]
+    tail = move_tail(head, [0, 0])
     assert tail == [1, 0]
 
-    head, tail = perform_move([1, 1], [0, 0], 'R')
+    head = move_head([1, 1], 'R')
     assert head == [2, 1]
+    tail = move_tail(head, [0, 0])
     assert tail == [1, 1]
 
-    head, tail = perform_move([1, 0], [0, 0], 'U')
+    head = move_head([1, 0], 'U')
     assert head == [1, 1]
+    tail = move_tail(head, [0, 0])
     assert tail == [0, 0]
 
 
