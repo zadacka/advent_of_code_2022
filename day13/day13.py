5b90749208dbccac55e6c21706f79db83fc666ae
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2022-12-13 21:04:52 +0000

    Day 13 AoC 2022
    Part 1 ... with thanks to
    https://www.reddit.com/user/zndflxtyh/
    
    Really annoyed to have mis-read/mis-understood the problem ... had have had a 'wrong' but passes tests initial attempt

--- day13/day13.py
+++ day13/day13.py
@@ -24,56 +24,48 @@ def test__load_pairs_input():
     assert actual == expected
 
 
-def check_correct_order(left, right, check_list_length=True):
+def check_correct_order(left, right):
     """
+    -1 is False, 1 is True, 0 is 'no comparison' (used by recursive calls)
     """
     if isinstance(left, int) and isinstance(right, int):
         # Rule 1: If both values are integers, the lower integer should come first.
-        if left > right:
-            return False
+        return 1 if left < right else -1 if left > right else 0
     elif isinstance(left, list) and isinstance(right, list):
         # Rule 2: If both values are lists keep checking... false if the right list runs out of items first
         # and no comparison makes a decision about the order, continue checking the next part of the input.
-        if check_list_length and len(right) < len(left):
-            return False
-        for l2, r2 in zip(left, right):
-            if check_correct_order(l2, r2) is False:
-                return False
+        for comparison_result in (check_correct_order(l, r) for l, r in zip(left, right)):
+            if comparison_result != 0:
+                return comparison_result
+        # THIS MESSED ME UP ... only use the list length check if element check has not been decisive
+        return -1 if len(right) < len(left) else 1 if len(left) < len(right) else 0
     elif isinstance(left, list) and isinstance(right, int):
         # Rule 3: If exactly one value is an integer, convert it to a list which contains that integer as its only value
-        if check_correct_order(left, [right], check_list_length=False) is False:
-            return False
+        return check_correct_order(left, [right])
     elif isinstance(left, int) and isinstance(right, list):
         # Rule 3: again
-        if check_correct_order([left], right, check_list_length=False) is False:
-            return False
+        return check_correct_order([left], right)
     else:
         raise RuntimeError("Some crazy input brought me here: l='{}', r='{}'".format(left, right))
 
-    return True
-
 
 def test__check_right_order():
-    assert check_correct_order([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])
-    assert check_correct_order([[1], [2, 3, 4]], [[1], 4])
-    assert not check_correct_order([9], [[8, 7, 6]])
-    assert check_correct_order([[4, 4], 4, 4], [[4, 4], 4, 4, 4])
-    assert not check_correct_order([7, 7, 7, 7], [7, 7, 7])
-    assert check_correct_order([], [3])
-    assert not check_correct_order([[[]]], [[]])
-    assert not check_correct_order([1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9])
+    assert 1 == check_correct_order([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])
+    assert 1 == check_correct_order([[1], [2, 3, 4]], [[1], 4])
+    assert -1 == check_correct_order([9], [[8, 7, 6]])
+    assert 1 == check_correct_order([[4, 4], 4, 4], [[4, 4], 4, 4, 4])
+    assert -1 == check_correct_order([7, 7, 7, 7], [7, 7, 7])
+    assert 1 == check_correct_order([], [3])
+    assert -1 == check_correct_order([[[]]], [[]])
+    assert -1 == check_correct_order([1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9])
 
 
 def count_correct_index_values(input_file):
     pairs = load_pairs_input(input_file)
     results = [check_correct_order(*pair) for pair in pairs]
-    result = 0
-    for index, check in enumerate(results, start=1):
-        if check:
-            result += index
+    result = sum(index for index, check in enumerate(results, start=1) if check == 1)
     return result
-
 
 def test__part1():
     assert 13 == count_correct_index_values('day13_test_input.txt')
-    assert 627 == count_correct_index_values('day13_real_input.txt')
+    assert 5366 == count_correct_index_values('day13_real_input.txt')
